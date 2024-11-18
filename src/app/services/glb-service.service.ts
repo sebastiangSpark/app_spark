b6f28dfc3111e5b483b2bdf612995608949d7990
Sebastian <sebastiang@sparkbenefits.biz>
2024-11-18 12:44:32 -0500

    update: cap add android

--- src/app/services/glb-service.service.ts
+++ src/app/services/glb-service.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { ActionPerformed, PushNotificationSchema, PushNotifications, Token } from '@capacitor/push-notifications';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlbServiceService {
 
-  constructor() { }
+  constructor(public platform: Platform) {
+    if(this.platform.is('capacitor')) this.initPushNotifications();
+  }
+
+  initPushNotifications() {
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      alert('Push received: ' + JSON.stringify(notification));
+    });
+
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+      alert('Push action performed: ' + JSON.stringify(notification));
+    });
+  }
 }
